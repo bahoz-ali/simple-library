715be577520b2465af2c888148a0cb42ee83367f
Bahoz <bahozali50@gmail.com>
2022-06-28 00:44:14 +0300

    implement the deletion of the book, fix the displayBooks function

--- scripts/index.js
+++ scripts/index.js
@@ -3,11 +3,6 @@ const authorInput = document.querySelector('#author_input');
 const bookList = document.querySelector('.book_list');
 const addBookButton = document.querySelector('#add_book');
 
-let id = 0;
-function updateId() {
-  id++;
-}
-
 class Book {
   constructor(id, title, authorName) {
     this.id = id;
@@ -17,18 +12,16 @@ class Book {
 }
 
 function init() {
-  if (localStorage.getItem('books')) {
-    return null;
+  if (!localStorage.getItem('books')) {
+    localStorage.setItem('books', JSON.stringify([]));
   }
-
-  localStorage.setItem('books', JSON.stringify([]));
 }
 
 function getData(key) {
   try {
     return JSON.parse(localStorage.getItem(key));
   } catch (error) {
-    return localStorage.getItem(key)
+    return localStorage.getItem(key);
   }
 }
 
@@ -38,7 +31,7 @@ function templateBook(obj) {
 
   const insideBook = `<p id="title">${obj.title}</p>
             <p id="author">${obj.authorName}</p>
-            <button type="submit" data-id='${obj.id}'>delete</button>`;
+            <button id="delete_book" type="submit" onClick="deleteBook(${obj.id})" data-id='${obj.id}'>delete</button>`;
 
   div.innerHTML = insideBook;
 
@@ -47,6 +40,9 @@ function templateBook(obj) {
 
 function displayBooks() {
   const books = getData('books');
+
+  // clean the display books then before.
+  bookList.innerHTML = '';
 
   if (books) {
     books.forEach((book) => {
@@ -63,19 +59,36 @@ function updateStorage(book) {
   displayBooks();
 }
 
+// eslint-disable-next-line
+function deleteBook(id) {
+  const books = getData('books');
+
+  if (books) {
+    const updateBooks = books.filter((b) => b.id !== id);
+
+    localStorage.setItem('books', JSON.stringify(updateBooks));
+    displayBooks();
+  }
+}
+
 function addBook(e) {
   e.preventDefault();
-  let title = titleInput.value.trim() ? titleInput.value.trim() : 'test';
-  let author = authorInput.value.trim() ? authorInput.value.trim() : 'test';
+
+  const form = document.querySelector('.book_section form');
+
+  const { length } = getData('books');
+  const id = length ? length + 1 : 0;
+  const title = titleInput.value.trim() ? titleInput.value.trim() : 'test';
+  const author = authorInput.value.trim() ? authorInput.value.trim() : 'test';
 
   const newBook = new Book(id, title, author);
 
   updateStorage(newBook);
-
-  updateId();
+  form.reset();
 }
 
 addBookButton.addEventListener('click', addBook);
+// deleteBookButton.addEventListener('click', deleteBook);
 
 document.addEventListener('DOMContentLoaded', () => {
   init();
