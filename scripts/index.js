59ae171fc32ce1da0cbbc4458a12cae8ba16eadf
Bahoz <bahozali50@gmail.com>
2022-06-27 22:52:55 +0300

    create collectin to store books, and display the books

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,55 @@
-import BookCollection from '../modules/class.js';
-import { currentDate, pageSwitcher } from '../modules/switcher.js';
-
 const bookTitle = document.querySelector('#title');
 const bookAuthor = document.querySelector('#author');
-const date = document.querySelector('#date');
-const booksStore = [];
+const bookList = document.querySelector('.book_list');
 
-// instatiate
-const Book1 = new BookCollection(bookTitle, autho, booksStore);
-Book1.saveData();
-Book1.removeData();
-Book1.existingData();
+let id = 0;
 
+class Book {
+  constructor(id, title, authorName) {
+    this.id = id;
+    this.title = title;
+    this.authorName = authorName;
+  }
+}
+
+const book = new Book(1, 'sad book', 'Chim');
+
+function init() {
+  if (localStorage.getItem('books')) {
+    return null;
+  }
+
+  localStorage.setItem('books', JSON.stringify([]));
+}
+
+function getData(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
+
+function templateBook(obj) {
+  const div = document.createElement('div');
+  div.classList.add('book');
+
+  const insideBook = `<p id="title">${obj.title}</p>
+            <p id="author">${obj.authorName}</p>
+            <button type="submit">delete</button>`;
+
+  div.innerHTML = insideBook;
+
+  bookList.append(div);
+}
+
+function displayBooks() {
+  const books = getData('books');
+
+  if (books) {
+    books.forEach((book) => {
+      templateBook(book);
+    });
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  init();
+  displayBooks();
+});
